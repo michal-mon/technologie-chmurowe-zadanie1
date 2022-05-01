20e9656a682648ff0be82b374761626fbc9aebc0
Michal Mon <mmon1235@gmail.com>
2022-05-01 20:42:32 +0200

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 //Import biblioteki "express" oraz "axios"
 const express = require("express");
 const axios = require("axios");
-
 //Utworzenie aplikacji Express
 const app = express();
-
-//Ustawienie stalej "port" jako port 3000
-const port = process.env.PORT | 3000;
+//Ustawienie stalej "port" jako port 5000
+const port = process.env.PORT | 5000;
 
 app.get("/", (req, res) => {
     // Pobranie adresu IP klienta
@@ -39,7 +37,7 @@ app.get("/", (req, res) => {
 });
 
 
-//Ustawienie nasluchiwania serwera na porcie 3000
+//Ustawienie nasluchiwania serwera na porcie 5000
 app.listen(port, () => {
   console.log("Michal Mon");
   console.log(`Data: ${Date()}`);
